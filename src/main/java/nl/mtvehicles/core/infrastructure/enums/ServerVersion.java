3b512c325e437d28d32488fe073f463ed070f2ff
Tiakin <killian.duboucher@outlook.fr>
2025-09-30 10:49:09 +0200

    Using movement handlers in ServerVersion enum

--- src/main/java/nl/mtvehicles/core/infrastructure/enums/ServerVersion.java
+++ src/main/java/nl/mtvehicles/core/infrastructure/enums/ServerVersion.java
@@ -1,6 +1,8 @@
 package nl.mtvehicles.core.infrastructure.enums;
 
 import nl.mtvehicles.core.infrastructure.annotations.VersionSpecific;
+import nl.mtvehicles.core.movement.PacketHandler;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -11,92 +13,113 @@ public enum ServerVersion {
     /**
      * 1.12-1.12.2
      */
-    v1_12_R1,
+    v1_12_R1(PacketHandler::movement_1_12),
     /**
      * 1.13.1-1.13.2
      */
-    v1_13_R2,
+    v1_13_R2(PacketHandler::movement_1_13),
     /**
      * 1.15-1.15.2
      */
-    v1_15_R1,
+    v1_15_R1(PacketHandler::movement_1_15),
     /**
      * 1.16.4-1.16.5
      */
-    v1_16_R3,
+    v1_16_R3(PacketHandler::movement_1_16),
     /**
      * 1.17-1.17.1
      */
-    v1_17_R1,
+    v1_17_R1(PacketHandler::movement_1_17),
     /**
      * 1.18-1.18.1
      */
-    v1_18_R1,
+    v1_18_R1(PacketHandler::movement_1_18_R1),
     /**
      * 1.18.2
      */
-    v1_18_R2,
+    v1_18_R2(PacketHandler::movement_1_18_R2),
     /**
      * 1.19-1.19.2
      * @since 2.4.3
      */
-    v1_19_R1,
+    v1_19_R1(PacketHandler::movement_1_19),
     /**
      * 1.19.3
      * @since 2.5.0
      */
-    v1_19_R2,
+    v1_19_R2(PacketHandler::movement_1_19_R2),
     /**
      * 1.19.4
      * @since 2.5.0
      */
-    v1_19_R3,
+    v1_19_R3(PacketHandler::movement_1_19_R3),
     /**
      * 1.20 and 1.20.1
      * @since 2.5.6
      */
-    v1_20_R1,
+    v1_20_R1(PacketHandler::movement_1_20_R1),
     /**
      * 1.20.2
      * @since 2.5.2
      */
-    v1_20_R2,
+    v1_20_R2(PacketHandler::movement_1_20_R2),
     /**
      * 1.20.3 and 1.20.4
      * @since 2.5.2
      */
-    v1_20_R3,
+    v1_20_R3(PacketHandler::movement_1_20_R3),
     /**
      * 1.20.5 and 1.20.6
      * @since 2.5.4
      */
-    v1_20_R4,
+    v1_20_R4(PacketHandler::movement_1_20_R4),
     /**
      * 1.21 and 1.21.1
      * @since 2.5.4
      */
-    v1_21_R1,
+    v1_21_R1(PacketHandler::movement_1_21_R1),
     /**
      * 1.21.2 and 1.21.3
      * @since 2.5.5
      */
-    v1_21_R2,
+    v1_21_R2(PacketHandler::movement_1_21_R2),
     /**
      * 1.21.4
      * @since 2.5.5
      */
-    v1_21_R3,
+    v1_21_R3(PacketHandler::movement_1_21_R3),
     /**
      * 1.21.5
      * @since 2.5.6
      */
-    v1_21_R4,
+    v1_21_R4(PacketHandler::movement_1_21_R4),
     /**
      * 1.21.6, 1.21.7 and 1.21.8
      * 
      * @since 2.5.6
      */
-    v1_21_R5;
+    v1_21_R5(PacketHandler::movement_1_21_R5);
+
+    /**
+     * Function interface for packet movement handlers
+     */
+    public interface MovementHandler {
+        void handle(Player player);
+    }
+
+    private final MovementHandler movementHandler;
+
+    ServerVersion(MovementHandler movementHandler) {
+        this.movementHandler = movementHandler;
+    }
+
+    /**
+     * Get the movement handler for this server version
+     * @return The movement handler for this version
+     */
+    public MovementHandler getMovementHandler() {
+        return this.movementHandler;
+    }
 
     public boolean is1_12_R1(){return this.equals(v1_12_R1);}
 
--- src/main/java/nl/mtvehicles/core/movement/MovementManager.java
+++ src/main/java/nl/mtvehicles/core/movement/MovementManager.java
@@ -14,24 +14,6 @@ public class MovementManager {
      */
     @VersionSpecific
     public static void MovementSelector(Player player) {
-        if (getServerVersion().is1_12_R1()) PacketHandler.movement_1_12(player);
-        else if (getServerVersion().is1_13_R2()) PacketHandler.movement_1_13(player);
-        else if (getServerVersion().is1_15_R1()) PacketHandler.movement_1_15(player);
-        else if (getServerVersion().is1_16_R3()) PacketHandler.movement_1_16(player);
-        else if (getServerVersion().is1_17_R1()) PacketHandler.movement_1_17(player);
-        else if (getServerVersion().is1_18_R1()) PacketHandler.movement_1_18_R1(player);
-        else if (getServerVersion().is1_18_R2()) PacketHandler.movement_1_18_R2(player);
-        else if (getServerVersion().is1_19_R1()) PacketHandler.movement_1_19(player);
-        else if (getServerVersion().is1_19_R2()) PacketHandler.movement_1_19_R2(player);
-        else if (getServerVersion().is1_19_R3()) PacketHandler.movement_1_19_R3(player);
-        else if (getServerVersion().is1_20_R1()) PacketHandler.movement_1_20_R1(player);
-        else if (getServerVersion().is1_20_R2()) PacketHandler.movement_1_20_R2(player);
-        else if (getServerVersion().is1_20_R3()) PacketHandler.movement_1_20_R3(player);
-        else if (getServerVersion().is1_20_R4()) PacketHandler.movement_1_20_R4(player);
-        else if (getServerVersion().is1_21_R1()) PacketHandler.movement_1_21_R1(player);
-        else if (getServerVersion().is1_21_R2()) PacketHandler.movement_1_21_R2(player);
-        else if (getServerVersion().is1_21_R3()) PacketHandler.movement_1_21_R3(player);
-        else if (getServerVersion().is1_21_R4()) PacketHandler.movement_1_21_R4(player);
-        else if (getServerVersion().is1_21_R5()) PacketHandler.movement_1_21_R5(player);
+        getServerVersion().getMovementHandler().handle(player);
     }
 }
